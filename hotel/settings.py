33cfd9bfd440c2d8b0c41a048e85850e97163ca0
tobiassdb1 <tobiassdb1@protonmail.com>
2021-07-05 15:11:05 -0300

    Adicionado pasta STATIC na raíz

--- hotel/settings.py
+++ hotel/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -122,7 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
